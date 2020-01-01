5219c21bd8637acfb50bff60160e43742b4ea061
Lian <nail3105@gmail.com>
2020-01-01 17:41:23 -0600

    Revert "working on loading data from file"
    This reverts commit ca2009d2cea071b130e69cf0bca257a8aa26d056.

--- main.go
+++ main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 	"path/filepath"
 	"time"
-	"io"
 )
 
 const ignore = ".goFileNsync"
-const lhistory_path = ignore + "/lhistory"
 
 type FileDesc struct {
 	Name    string
@@ -20,7 +17,7 @@ type FileDesc struct {
 
 func main() {
 
-	dir := "./test"
+	dir := "."
 
 	files, err := lsDir(dir)
 
@@ -28,95 +25,14 @@ func main() {
 		panic(err)
 	}
 
-	var fds []FileDesc
-
 	for _, file := range files {
-		fmt.Println("Name: ", file.Name())
-		fmt.Println("Mode Time: ", file.ModTime())
-		fmt.Println("Size: ", file.Size())
-		fds = append(fds, New(file))
+		fd := New(file)
+
+		fmt.Println("Name: ", fd.Name)
+		fmt.Println("Mode Time: ", fd.ModTime)
+		fmt.Println("Size: ", fd.Size)
 	}
 
-	mkDir()
-	err = saveHistory(fds)
-
-	if err != nil {
-		panic(err)
-	}
-
-	lfds, err := loadHistory()
-
-	if err != nil {
-		panic(err)
-	}
-
-	for _, file := range lfds {
-		fmt.Println("Name: ", file.Name)
-		fmt.Println("Mode Time: ", file.ModTime)
-		fmt.Println("Size: ", file.Size)
-	}
-}
-
-func saveHistory(files []FileDesc) error {
-
-	f, err := os.Create(lhistory_path)
-
-	if err != nil {
-		return err
-	}
-
-	for _, fd := range files {
-		s := fmt.Sprintf("%s %s %d\n", fd.Name, fd.ModTime, fd.Size)
-		_, err := f.WriteString(s)
-
-		if err != nil {
-			fmt.Println("Error writing file named %s data", fd.Name)
-		}
-	}
-
-	return nil
-}
-
-func loadHistory() ([]FileDesc, error) {
-
-	var files []FileDesc
-
-	file, err := os.Open(lhistory_path)
-	defer file.Close()
-
-	if err != nil {
-		return nil, err
-	}
-
-	reader := bufio.NewReader(file)
-
-	var line string
-
-	var name string
-	var modTime time.Time
-	var size int
-
-	for {
-		line, err = reader.ReadString('\n')
-
-		fmt.Sscan(line, &name, &modTime, &size)
-		fd := FileDesc{
-			Name:    name,
-			ModTime: modTime,
-			Size:    size,
-		}
-		files = append(files, fd)
-
-		if err != nil {
-			break
-		}
-	}
-
-	if err != io.EOF {
-        return nil, err
-    }
-
-	return files, nil
 }
 
 func New(f os.FileInfo) FileDesc {
